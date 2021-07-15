3f903b63542b660530e19c855ccb457c8d74790f
jkpark104 <jkrang104@gmail.com>
2021-07-15 23:54:20 +0900

    TIL js JSON

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-// JS Object
-// Static Method : Parameters를 필요치 않음
-// 특정 인스턴스와 연결 돼 있지 않음
+import axios from 'axios'
 
-let target = { a: 1, b: 2}
-let source = { b: 4, c: 5}
+function fetchMovies() {
+  axios
+  .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+  .then(res => {
+    const h1El = document.querySelector('h1')
+    const imgEl = document.querySelector('img')
+    h1El.textContent = res.data.Search[0].Title
+    imgEl.src = res.data.Search[0].Poster
+  })
+}
 
-// 1. assign(a,b) b를 a에 병합하고 a를 반환
-let returnedTarget = Object.assign(target, source)
-
-console.log(target == returnedTarget)
-
-const a1 = {k:123}
-const a2 = {k:123}
-console.log(a1===a2)
-// console.log(a1==a2) -> false -> why?
-
-// 2. assign({}, a, b) a, b를 합쳐 새로운 객체 생성
-
-// 3. keys
-console.log(Object.keys(target))
-let values = Object.keys(target).map(key => target[key])
-console.log(values)
-// values 리턴
-
-// 4. 
+fetchMovies()
---
+++ printHi.js
@@ -0,0 +1,5 @@
+export function printHi() {
+  console.log('hi')
+}
+
+export const tmp = 3
