beea1814c389efd23bce7e612b4b48df78aa621d
jkpark104 <jkrang104@gmail.com>
2021-07-16 10:47:06 +0900

    JS 학습 1일차

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-import axios from 'axios'
-
-function fetchMovies() {
-  axios
-  .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
-  .then(res => {
-    const h1El = document.querySelector('h1')
-    const imgEl = document.querySelector('img')
-    h1El.textContent = res.data.Search[0].Title
-    imgEl.src = res.data.Search[0].Poster
-  })
-}
-
-fetchMovies()
+const name = 'park'
+console.log(`Hello ${name}`)
